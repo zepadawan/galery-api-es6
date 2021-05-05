4f15a57dadfc801835dfa55eca28200ec5165fa4
JF PETROFF <zepadawan@gmail.com>
2021-05-05 14:35:27 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import bodyPrser from 'body-parser';
+import cors from 'cors';
+import passport from 'passport';
+import errorHandler from './src/_middleware/error-handler.js';
+import config from './src/configs/config.js';
+
+const app = express();
+app.use(cors());
+app.use(bodyPrser.json());
+app.use(express.static('./assets'));
+app.use(passport.initialize());
+
+import authorize from './src/_middleware/autorize.js';
+
+import router from './src/routes/user.route.js';
+app.use('/users', router);
+
+
+app.get('/', (req, res) => res.send('Hello my World'));
+let port = config.server.portServer || 4000;
+//create a server
+var server = app.listen(port, function () {
+  var host = config.server.portServer;
+  var port = config.server.host;
+  console.log(`App listening at http:// ${port} : ${host} `);
+});
+
+
+
+
+
+
+
